cbefeb39c3abde5905006a2b7c7bff7c08c6ba86
Kosei-Yamauchi <koseyama560@gmail.com>
2026-03-25 13:23:32 +0900

    ver11.0

--- data.js
+++ data.js
@@ -1,13 +1,24 @@
 /* data.js */
 const PROJECT_DATA = {
     commissioned: [
-        { id: "Echoes_Eye", src: "images/Echoes_Eye.jpg", cap: "Tentative-Echoes Vol.1" },
-        { id: "Echoes_Ear-2", src: "images/Echoes_Ear-2.jpg", cap: "Tentative-Echoes Vol.1" },
-        { id: "Popeye", src: "images/Popeye_JapaneseBreakfast-0327.jpg", cap: "Popeye Web Popeye_Japanese Breakfast-001" }
+        { id: "Echoes_Eye", src: "images/Echoes_Eye.jpg", cap: "Tentative, Echoes Vol.1 Eye" },
+        { id: "Echoes_Ear-2", src: "images/Echoes_Ear-2.jpg", cap: "Tentative, Echoes Vol.1 Ear" },
+        { id: "Popeye_001", src: "images/Popeye_JapaneseBreakfast-0327.jpg", cap: "Popeye Web, Japanese Breakfast-001" },
+        { id: "Popeye_002", src: "images/Popeye_JapaneseBreakfast-0431.jpg", cap: "Popeye Web, Japanese Breakfast-002" },
+        { id: "Popeye_003", src: "images/Popeye_JapaneseBreakfast-0468.jpg", cap: "Popeye Web, Japanese Breakfast-003" },
         // 以降、ここに追加するだけで全ページに反映されます
     ],
     personal: [
-        { id: "srilanka-3397", src: "images/Personal_SriLanka-3397.jpg", cap: "Sri Lanka" },
-        { id: "germany-4653", src: "images/Germany-4653.jpg", cap: "Germany" }
+        { id: "srilanka-3397", src: "images/Personal_SriLanka-3397.jpg", cap: "Landscape, Asia-001" },
+        { id: "srilanka-2876", src: "images/Personal_SriLanka-2876.jpg", cap: "Landscape, Asia-002" },
+        { id: "germany-4653", src: "images/Germany-4653.jpg", cap: "Landscape, Europe-001" },
+        { id: "germany-4667", src: "images/Germany-4667.jpg", cap: "Architecture, Europe-001" },
+        { id: "germany-4686", src: "images/Germany-4686.jpg", cap: "Architecture, Europe-002" },
+        { id: "germany-4699", src: "images/Germany-4699.jpg", cap: "Architecture, Europe-003" },
+        { id: "UK-4389", src: "images/UK-4389.jpg", cap: "Landscape, Europe-002" },
+        { id: "UK-4409", src: "images/UK-4409.jpg", cap: "Landscape, Europe-003" },
+        { id: "UK-4416", src: "images/UK-4416.jpg", cap: "Landscape, Europe-004" },
+        { id: "UK-4419", src: "images/UK-4419.jpg", cap: "Landscape, Europe-005" },
+        { id: "UK-4465", src: "images/UK-4465.jpg", cap: "Landscape, Europe-006" },
     ]
 };
