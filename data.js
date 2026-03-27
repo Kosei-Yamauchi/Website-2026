0034bf31f1cedc950f88f75e76b866b1a5913465
Kosei-Yamauchi <koseyama560@gmail.com>
2026-03-27 23:56:17 +0900

    ver17.0

--- data.js
+++ data.js
@@ -5,7 +5,15 @@ const PROJECT_DATA = {
         { project: "Tentative, Echoes", id: "Echoes_Ear-2", src: "images/Echoes_Ear-2.jpg", cap: "Vol.1 Ear" },
         { project: "Popeye Web, Japanese Breakfast", id: "Popeye_001", src: "images/Popeye_JapaneseBreakfast-0327.jpg", cap: "Japanese Breakfast-001" },
         { project: "Popeye Web, Japanese Breakfast", id: "Popeye_002", src: "images/Popeye_JapaneseBreakfast-0431.jpg", cap: "Japanese Breakfast-002" },
-        { project: "Popeye Web, Japanese Breakfast", id: "Popeye_003", src: "images/Popeye_JapaneseBreakfast-0468.jpg", cap: "Japanese Breakfast-003" }
+        { project: "Popeye Web, Japanese Breakfast", id: "Popeye_003", src: "images/Popeye_JapaneseBreakfast-0468.jpg", cap: "Japanese Breakfast-003" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_001", src: "images/OAO-Exhibition-4310.jpg", cap: "OAO 2026 Spring/Summer Preview-001" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_002", src: "images/OAO-Exhibition-4343.jpg", cap: "OAO 2026 Spring/Summer Preview-002" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_003", src: "images/OAO-Exhibition-4398.jpg", cap: "OAO 2026 Spring/Summer Preview-003" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_004", src: "images/OAO-Exhibition-4413.jpg", cap: "OAO 2026 Spring/Summer Preview-004" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_005", src: "images/OAO-Exhibition-4421.jpg", cap: "OAO 2026 Spring/Summer Preview-005" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_006", src: "images/OAO-Exhibition-4480.jpg", cap: "OAO 2026 Spring/Summer Preview-006" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_007", src: "images/OAO-Exhibition-4509.jpg", cap: "OAO 2026 Spring/Summer Preview-007" },
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_008", src: "images/OAO-Exhibition-4538.jpg", cap: "OAO 2026 Spring/Summer Preview-008" }
     ],
     personal: [
         { project: "Landscape, Asia", id: "srilanka-3397", src: "images/Personal_SriLanka-3397.jpg", cap: "Landscape, Asia-001" },
@@ -18,6 +26,11 @@ const PROJECT_DATA = {
         { project: "Landscape, Europe", id: "UK-4409", src: "images/UK-4409.jpg", cap: "Landscape, Europe-003" },
         { project: "Landscape, Europe", id: "UK-4416", src: "images/UK-4416.jpg", cap: "Landscape, Europe-004" },
         { project: "Landscape, Europe", id: "UK-4419", src: "images/UK-4419.jpg", cap: "Landscape, Europe-005" },
-        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4465.jpg", cap: "Landscape, Europe-006" }
+        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4465.jpg", cap: "Landscape, Europe-006" },
+        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4384.jpg", cap: "Landscape, Europe-007" },
+        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4397.jpg", cap: "Landscape, Europe-008" },
+        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4439.jpg", cap: "Landscape, Europe-009" },
+        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4490.jpg", cap: "Landscape, Europe-010" },
+        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4496.jpg", cap: "Landscape, Europe-011" }
     ]
 };
