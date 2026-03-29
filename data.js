548220e60f4617b86e07f87b342fe705b08d8567
Kosei-Yamauchi <koseyama560@gmail.com>
2026-03-29 16:05:48 +0900

    ver17.1

--- data.js
+++ data.js
@@ -1,5 +1,25 @@
 /* data.js */
 const PROJECT_DATA = {
+    personal: [
+        { project: "Stones", id: "Stones-4656", src: "images/Personal_Stones-4656.jpg", cap: "Stones-001" },
+        { project: "Stones", id: "Stones-4658", src: "images/Personal_Stones-4658.jpg", cap: "Stones-002" },
+        { project: "Landscape, Asia", id: "srilanka-3397", src: "images/Personal_SriLanka-3397.jpg", cap: "Landscape, Asia-001" },
+        { project: "Landscape, Asia", id: "srilanka-2876", src: "images/Personal_SriLanka-2876.jpg", cap: "Landscape, Asia-002" },
+        { project: "Landscape, Europe", id: "germany-4653", src: "images/Germany-4653.jpg", cap: "Landscape, Europe-001" },
+        { project: "Landscape, Europe", id: "germany-4667", src: "images/Germany-4667.jpg", cap: "Architecture, Europe-001" },
+        { project: "Landscape, Europe", id: "germany-4686", src: "images/Germany-4686.jpg", cap: "Architecture, Europe-002" },
+        { project: "Landscape, Europe", id: "germany-4699", src: "images/Germany-4699.jpg", cap: "Architecture, Europe-003" },
+        { project: "Landscape, Europe", id: "UK-4389", src: "images/UK-4389.jpg", cap: "Landscape, Europe-002" },
+        { project: "Landscape, Europe", id: "UK-4409", src: "images/UK-4409.jpg", cap: "Landscape, Europe-003" },
+        { project: "Landscape, Europe", id: "UK-4416", src: "images/UK-4416.jpg", cap: "Landscape, Europe-004" },
+        { project: "Landscape, Europe", id: "UK-4419", src: "images/UK-4419.jpg", cap: "Landscape, Europe-005" },
+        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4465.jpg", cap: "Landscape, Europe-006" },
+        { project: "Landscape, Europe", id: "UK-4384", src: "images/UK-4384.jpg", cap: "Landscape, Europe-007" },
+        { project: "Landscape, Europe", id: "UK-4397", src: "images/UK-4397.jpg", cap: "Landscape, Europe-008" },
+        { project: "Landscape, Europe", id: "UK-4439", src: "images/UK-4439.jpg", cap: "Landscape, Europe-009" },
+        { project: "Landscape, Europe", id: "UK-4490", src: "images/UK-4490.jpg", cap: "Landscape, Europe-010" },
+        { project: "Landscape, Europe", id: "UK-4496", src: "images/UK-4496.jpg", cap: "Landscape, Europe-011" }
+    ],
     commissioned: [
         { project: "Tentative, Echoes", id: "Echoes_Eye", src: "images/Echoes_Eye.jpg", cap: "Vol.1 Eye" },
         { project: "Tentative, Echoes", id: "Echoes_Ear-2", src: "images/Echoes_Ear-2.jpg", cap: "Vol.1 Ear" },
@@ -13,24 +33,11 @@ const PROJECT_DATA = {
         { project: "OAO 2026 Spring-Summer Preview", id: "OAO_005", src: "images/OAO-Exhibition-4421.jpg", cap: "OAO 2026 Spring/Summer Preview-005" },
         { project: "OAO 2026 Spring-Summer Preview", id: "OAO_006", src: "images/OAO-Exhibition-4480.jpg", cap: "OAO 2026 Spring/Summer Preview-006" },
         { project: "OAO 2026 Spring-Summer Preview", id: "OAO_007", src: "images/OAO-Exhibition-4509.jpg", cap: "OAO 2026 Spring/Summer Preview-007" },
-        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_008", src: "images/OAO-Exhibition-4538.jpg", cap: "OAO 2026 Spring/Summer Preview-008" }
-    ],
-    personal: [
-        { project: "Landscape, Asia", id: "srilanka-3397", src: "images/Personal_SriLanka-3397.jpg", cap: "Landscape, Asia-001" },
-        { project: "Landscape, Asia", id: "srilanka-2876", src: "images/Personal_SriLanka-2876.jpg", cap: "Landscape, Asia-002" },
-        { project: "Landscape, Europe", id: "germany-4653", src: "images/Germany-4653.jpg", cap: "Landscape, Europe-001" },
-        { project: "Landscape, Europe", id: "germany-4667", src: "images/Germany-4667.jpg", cap: "Architecture, Europe-001" },
-        { project: "Landscape, Europe", id: "germany-4686", src: "images/Germany-4686.jpg", cap: "Architecture, Europe-002" },
-        { project: "Landscape, Europe", id: "germany-4699", src: "images/Germany-4699.jpg", cap: "Architecture, Europe-003" },
-        { project: "Landscape, Europe", id: "UK-4389", src: "images/UK-4389.jpg", cap: "Landscape, Europe-002" },
-        { project: "Landscape, Europe", id: "UK-4409", src: "images/UK-4409.jpg", cap: "Landscape, Europe-003" },
-        { project: "Landscape, Europe", id: "UK-4416", src: "images/UK-4416.jpg", cap: "Landscape, Europe-004" },
-        { project: "Landscape, Europe", id: "UK-4419", src: "images/UK-4419.jpg", cap: "Landscape, Europe-005" },
-        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4465.jpg", cap: "Landscape, Europe-006" },
-        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4384.jpg", cap: "Landscape, Europe-007" },
-        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4397.jpg", cap: "Landscape, Europe-008" },
-        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4439.jpg", cap: "Landscape, Europe-009" },
-        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4490.jpg", cap: "Landscape, Europe-010" },
-        { project: "Landscape, Europe", id: "UK-4465", src: "images/UK-4496.jpg", cap: "Landscape, Europe-011" }
+        { project: "OAO 2026 Spring-Summer Preview", id: "OAO_008", src: "images/OAO-Exhibition-4538.jpg", cap: "OAO 2026 Spring/Summer Preview-008" },
+        { project: "Perfume Oil Factory", id: "POF-1", src: "images/POF-1.jpg", cap: "Perfume Oil Factory-001" },
+        { project: "Perfume Oil Factory", id: "POF-2", src: "images/POF-2.jpg", cap: "Perfume Oil Factory-002" },
+        { project: "Perfume Oil Factory", id: "POF-3", src: "images/POF-3.jpg", cap: "Perfume Oil Factory-003" },
+        { project: "Perfume Oil Factory", id: "POF-4", src: "images/POF-4.jpg", cap: "Perfume Oil Factory-004" },
+        { project: "Perfume Oil Factory", id: "POF-5", src: "images/POF-5.jpg", cap: "Perfume Oil Factory-005" },
     ]
 };
